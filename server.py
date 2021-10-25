b37c7a0ee913176a4c3b9e31b83a8a23edb8e05d
Bella and Mehayla <mehaylac@gmail.com>
2021-10-25 20:44:47 +0000

    started jsx & made new folders

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
+""" Server for upstatement app"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def show_home():
-    return render_template("main.html")
+    """ Landing and home page """
+    return render_template('main.html')
+
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", debug=True)
---
+++ slide.jsx
@@ -0,0 +1,8 @@
+
+function Hello() {
+    return (
+        <p>Inseret PMFB here</p>
+    );
+}
+
+ReactDOM.render(<Hello />, document.querySelector('#carousel'));
